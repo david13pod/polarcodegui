fc14e987ab35c0c676ab34eefaebbbc47eabb137
Oluwapelumi <odedereoluwapelumi13@gmail.com>
2021-11-02 01:17:58 +0100

    Update

--- complex_sim.py
+++ complex_sim.py
@@ -3,23 +3,41 @@ from polarcodes import *
 import statistics as st
 import time
 
+# System input
+msg_len=[180,180,180]
+channel='ucl'  # ucl for uplink channel and dcl for downlink channel
+desired_rate=[0.16, 0.3, 0.7]
+use_ratematching=True #Activate rate matching
+sim_time=5000 # number of blocks
+List_n = 4 # list size for SCL decoder
+design_SNR  = Sim_utils.snr_gen(0,28,0.2)
 
+
+
+# myPC = PolarCode(N, M, k, shorten_params)
 start_time2 = time.time()
 
-msg_len=[30] # information length
-crc_n=Sim_utils.crc_selector(msg_len,'ucl') # crc polynomial generator length
+msg_len=msg_len # information length
+crc_n=Sim_utils.crc_selector(msg_len,channel) # crc polynomial generator length
 k=list(crc_n+np.array(msg_len)) # crc coded information length K
-N=[128] # mothercode N
-M=[100] #codeword E
-match_rate=list(np.array(k)/np.array(M)) # K/E
-rate=list(np.array(msg_len)/np.array(M)) # A/E
-design_SNR  = Sim_utils.snr_gen(-0.2,30,.2)
+rate=desired_rate # A/E desired system rate
+M=(np.array(msg_len)/np.array(rate))  #codeword E = A/R
+for ii in range(0,len(msg_len)):
+    if k[ii] >= M[ii]:
+        raise ValueError(f'crc coded information {k[ii]} can not be greater than the codeword {M[ii]} in length')
+M=list(M.astype(int))
+N = Sim_utils.mothercode(channel,k,M) # mothercode N
+useRM=use_ratematching # Rate matching activator: True To activate rate matching False to deactivate. M == N when rate matching is not used 
+if useRM == False:
+    M=N  # no rate matching M == N
+match_rate=list(np.array(k)/np.array(M)) # match rate K/E
+design_SNR  = design_SNR # for bler plot
 construction='5g' # construction type
-sim_time=10 # number of blocks
-useRM=1 # Rate matching activator: 1 To activate rate matching zero to deactivate. M == N when rate matching is not used 
-List_n = 4 # list size for SCL decoder
+sim_time=sim_time # number of blocks
+List_n = List_n # list size for SCL decoder
 BER_avged=[]
 BLER_list=[]
+#time_list=[]
 for snr in design_SNR:
     BER=[]
     BLER=[]
@@ -29,6 +47,8 @@ for snr in design_SNR:
         bler_temp=0
         myPC=Sim_utils.matching_scheme_selector(N[i],M[i],k[i],match_rate[i],useRM,snr,construction)
         # print(myPC, "\n\n")
+        #time_temp=[]
+        #start_time3 = time.time()
             
         for blk in range(0,sim_time):
 
@@ -57,13 +77,16 @@ for snr in design_SNR:
             ber_temp=[st.mean(ber_temp)]
             if errors > 0: # cal bler
                 bler_temp+=1
+        #time_temp.append(time.time() - start_time3)
         BER.append(float(ber_temp[0]))
         BLER.append(bler_temp/sim_time)
 
     BER_avged.append(BER)
     BLER_list.append(BLER)
+    #time_list.append(time_temp)
 
 print( time.time() - start_time2, "seconds")
 # BLER_list
 
+# Plot results
 Sim_utils.sim_plot(BLER_list,design_SNR,msg_len,N,rate,match_rate,List_n)
--- polarcodes/Sim_utils.py
+++ polarcodes/Sim_utils.py
@@ -45,25 +45,42 @@ def crc_selector(A,mode):
             crcn.append(24)
     return crcn
 
+def mothercode(channel,k,M):
+    #implementation of mothercode length
+    N=[]
+    nmin=5
+    for i in range(0,len(k)):
+        n1=np.log2(M[i])
+        n2=np.log2(8*k[i])
+        if channel =='dcl':
+            nmax=9
+            n=max(min(n1,n2,nmax),nmin)
+            N.append(2**int(np.ceil(n)))
+        else:
+            nmax=10
+            n=max(min(n1,n2,nmax),nmin)
+            N.append(2**int(np.ceil(n)))
+    return N
+
 def matching_scheme_selector(N,M,k,match_rate,useRM,snr,construction):
-    if M==N and useRM==0: #no rate matching used
+    if M==N and useRM==False: #no rate matching used
         myPC = PolarCode(N,M, k)
         myPC.construction_type = construction
         Construct(myPC, snr)
         return myPC
-    elif M <= N and match_rate > (7/16) and useRM==1: #shortening
+    elif M <= N and match_rate > (7/16) and useRM==True: #shortening
         params = ('shorten', 'fiveG_shortnening', None, None, False)
         myPC = PolarCode(N,M, k, params)
         myPC.construction_type = construction
         Shorten(myPC, snr)
         return myPC
-    elif M <= N and match_rate <= (7/16) and useRM==1: #puncturing
+    elif M <= N and match_rate <= (7/16) and useRM==True: #puncturing
         params = ('punct', None, None, None, False)
         myPC = PolarCode(N,M, k, params)
         myPC.construction_type = construction
         Puncture(myPC, snr)
         return myPC
-    elif M > N and useRM==1: #repetition
+    elif M > N and useRM==True: #repetition
         params = ('rep', None, None, None, False)
         myPC = PolarCode(N,M, k, params)
         myPC.construction_type = construction
@@ -78,9 +95,9 @@ def sim_plot(BLER_list,design_SNR,msg_len,N,rate,match_rate,List_n):
 
     fig, ax = plt.subplots(figsize=(11,7))
     for kk in range(0,len(blertx)):
-    # for kk in [0,1,2]:
+    # for kk in [0,1,3,5]:
         plt.plot(design_SNR,blertx[kk], label='rate ='+str(round(rate[kk],2))+' , '+"msg_len ="
-        +str(msg_len[kk])+ ' , '+"N =" +str(N[kk])+", K/E =" +str(round(match_rate[kk],2)))
+        +str(msg_len[kk])+ ' , '+"N =" +str(N[kk])+", L =List_n")
 
     #plt.axis([0, 20, 0, 3.0e-1])
     plt.xscale('linear')
@@ -91,7 +108,10 @@ def sim_plot(BLER_list,design_SNR,msg_len,N,rate,match_rate,List_n):
     plt.xlabel('EbNo')
     plt.ylabel('BLER')
     plt.legend(loc='best')
-    plt.figtext(.51, .9, "rates: Rep<0.273<Punct<0.378<Short, L = "+str(List_n))
+    plt.figtext(.51, .9, "L = "+str(List_n))
+    plt.figtext(.2, .85, "rate matching scheme: Repetition: E>N, Punctuting E<N and R<0.438, Shortening E<N and R>0.438")
+    #plt.figtext(.51, .9, "rates: Rep<0.273<Punct<0.378<Short, L = "+str(List_n))
     plt.grid(True,which="both")
     plt.show()
-    fig.savefig('EbNo_vs_BLER_Compx_rateL8.jpg', bbox_inches='tight')
+    #fig.savefig('EbNo_vs_BLER_Compx_rateL8.jpg', bbox_inches='tight')
+    # fig.savefig('EbNo_vs_BLER_Comp_5g.jpg', bbox_inches='tight')
